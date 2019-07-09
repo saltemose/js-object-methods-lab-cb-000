94ddbe71dca73cb2e3079ddcbb3d459ddd1fc526
Sean Altemose <sean@seanaltemose.com>
2019-07-09 21:30:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // code solution here
 class President {
-  constructor(name, politicalParty, yearsInOffice, homeState)
+  constructor(name, politicalParty, yearsInOffice, homeState) {
+    this.name = name;
+    this.politicalParty = politicalParty;
+    this.yearsInOffice = yearsInOffice;
+    this.homeState = homeState;
+  }
 }
